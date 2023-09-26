a807618ef9483e3d54f04278d9f5e6c82155ea56
Santhosh <PSanthosh@twu.edu>
2023-09-26 11:47:41 -0500

    committing arrayex

---
+++ arrayex.pl
@@ -0,0 +1,42 @@
+# Array Exercises
+
+=begin
+
+pez.pl instructions: 
+
+1. Show Pez dispenser is empty
+2. Ask for 10 candy flavors
+3. Show Pez dispenser is full (display all 10 favors at once)
+4. Dispense Pez dispenser (act like a STACK data structure)
+5. Show Pez dispenser is empty
+
+Example of PART of the output:
+
+Full Pez: 0 1 2 3 4 5 6 7 8 9
+
+0123456789
+012345678
+01234567
+0123456
+012345
+01234
+0123
+012
+01
+0
+
+SUBMISSION: 
+pez.pl should be in your Assignments folder in your GITHUB repository
+PUSH and COMMIT to your repository
+
+=cut
+
+=begin
+
+Drill #1
+
+1. Ask the user for 5 items and put them in an array
+2. Print out the entire contents of the array
+3. Remove the FIRST two items in the array and put them at the END or the array
+
+=cut
